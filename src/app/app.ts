17d09f37df1db33670386cee2681fc6c9038968e
ziadhanii <UG_31107103@ics.tanta.edu.eg>
2025-12-19 18:33:04 +0200

    feat: add http client support

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +8,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('Dating-App');
+export class App implements OnInit {
+  private http = inject(HttpClient);
+  protected title = 'Dating-App';
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/members')
+      .subscribe({
+        next: response => console.log(response),
+        error: err => console.log(err),
+        complete: () => console.log('Request completed')
+      });
+  }
+
 }
