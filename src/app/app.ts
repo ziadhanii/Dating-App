e9efd7bbcf5806b2488eb8c0dc032caf046b9f58
ziadhanii <UG_31107103@ics.tanta.edu.eg>
2025-12-31 19:29:36 +0200

    feat: integrate Tailwind CSS and DaisyUI for styling
    - Added Tailwind CSS and DaisyUI to package.json dependencies.
    - Updated app.html to utilize Tailwind CSS classes for layout and styling.
    - Refactored app.ts to use signals for managing members data.
    - Introduced async/await for fetching members from the API.
    - Updated index.html to improve structure and title.
    - Added SSL certificates for local development.
    - Included user avatar image in the public directory.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,23 +1,33 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
+
+  protected members = signal<any[]>([]);
+
   private http = inject(HttpClient);
-  protected title = 'Dating-App';
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/members')
-      .subscribe({
-        next: response => console.log(response),
-        error: err => console.log(err),
-        complete: () => console.log('Request completed')
-      });
+  protected title = 'Dating App';
+
+
+  async ngOnInit() {
+    this.members.set(await this.getMembers());
   }
 
+
+  async getMembers() {
+    try {
+      return lastValueFrom(this.http.get<any[]>('https://localhost:5001/api/members'));
+    } catch (error) {
+      console.error('Error fetching members:', error);
+      throw error;
+    }
+
+  }
 }
